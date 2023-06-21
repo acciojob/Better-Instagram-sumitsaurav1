4c26d99e962fdecc9021b4584cfb0e12dff5610a
Sumit saurav <117914094+sumitsaurav1@users.noreply.github.com>
2023-06-21 17:53:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //your code here
+let images = document.querySelectorAll(".image");
+
+// console.log(images)
+
+let selectedDiv=null;
+
+function handleDragStart(){
+  selectedDiv=this;
+  this.classList.add("selected");
+}
+
+function handleDragOver(event){
+    event.preventDefault();
+    // console.log(event);
+}
+
+function handleDrop(e) {
+  if (selectedDiv !== this) {
+    let temp = window.getComputedStyle(selectedDiv).backgroundImage;
+    selectedDiv.style.backgroundImage = window.getComputedStyle(this).backgroundImage;
+    this.style.backgroundImage = temp;
+  }
+  selectedDiv.classList.remove('selected');
+  selectedDiv = null;
+}
+
+
+
+function doDrag(){
+  images.forEach((item)=>{
+    item.addEventListener('dragstart',handleDragStart)
+    item.addEventListener('dragover',handleDragOver)
+    item.addEventListener('drop',handleDrop)
+  })
+}
+
+
+doDrag()
